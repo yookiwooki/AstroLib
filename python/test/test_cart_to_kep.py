c8052eae885aca09ba9ac6263659092989c6e2ba
Sean <seanmcardle14@gmail.com>
2018-08-19 00:02:21 -0400

    Shortening unit test names for better console output.

--- python/test/test_cart_to_kep.py
+++ python/test/test_cart_to_kep.py
@@ -14,11 +14,12 @@ from orbits.cart_to_kep import rv2coe
 
 class TestCartToKep(unittest.TestCase):
     def setUp(self):
+        self.longMessage = True
         self.mu1 = 1.0 # Canonical (LU^3/sec^2)
         self.mu2 = 398600.4415 # Earth (km^3/sec^2)
         self.tol = 1e-3 # Error tolerance
 
-    def test_noninclined_nonequatorial_elliptical(self):
+    def test_noninc_noneq_ellip(self):
         rv = np.array([-0.35449, 1.3951 , -0.43398, \
                 0.46978, 0.86112, -0.18631])
         coe_ref  = np.array([3.0, 0.815, 2.84563, \
@@ -29,7 +30,7 @@ class TestCartToKep(unittest.TestCase):
                 'rv2coe fails for non-inclined, non-equatorial, \
                 elliptical orbit (default)')
 
-    def test_elliptical_equatorial(self):
+    def test_ellip_eq(self):
         rv = np.array([1, 0.25, 0, 0.1, 0.5, 0])
         coe_ref  = np.array([0.59513713, 0.78796300, 0.0, \
                              0.0, 0.0, 3.51853886 ])
@@ -38,7 +39,7 @@ class TestCartToKep(unittest.TestCase):
         self.assertTrue(error < self.tol,
                 'rv2coe fails for elliptical equatorial orbit')
 
-    def test_circular_inclined(self):
+    def test_circ_inc(self):
         rv = np.array([-np.sqrt(2), np.sqrt(2), 0, \
             -np.sqrt(2)/4, -np.sqrt(2)/4, 0.5])
         coe_ref  = np.array([2.0, 0.0, 0.785398, \
@@ -48,7 +49,7 @@ class TestCartToKep(unittest.TestCase):
         self.assertTrue(error < self.tol,
                 'rv2coe fails for circular inclined orbit')
 
-    def test_circular_equatorial(self):
+    def test_circ_eq(self):
         rv = np.array([21000*np.sqrt(2), 21000*np.sqrt(2), 0, \
                       -0.05*np.sqrt(self.mu2/210.0), \
                        0.05*np.sqrt(self.mu2/210.0), 0 ])
